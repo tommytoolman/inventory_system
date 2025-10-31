c25a3a1ba339a9a6add6ab87fc64ef19d773f7c0
Tommy Toolman <tommy.toolman@protonmail.com>
2025-10-31 18:22:59 +0000

    Avoid downgrading product images from partial Reverb payloads

--- app/services/reverb_service.py
+++ app/services/reverb_service.py
@@ -680,19 +680,28 @@ class ReverbService:
                         photo_urls.append(link)
 
             if photo_urls:
-                current_additional = product.additional_images or []
-                if (
-                    product.primary_image != photo_urls[0]
-                    or list(current_additional) != photo_urls[1:]
-                ):
-                    product.primary_image = photo_urls[0]
-                    product.additional_images = photo_urls[1:]
+                current_additional = list(product.additional_images or [])
+                existing_count = int(bool(product.primary_image)) + len(current_additional)
+                if len(photo_urls) < max(existing_count, 1):
                     logger.info(
-                        "Updated product %s media from Reverb listing %s",
+                        "Skipping media update for product %s; Reverb returned %s images (< existing %s)",
                         product.sku,
-                        reverb_id,
+                        len(photo_urls),
+                        existing_count,
                     )
-                    await self.db.flush()
+                else:
+                    if (
+                        product.primary_image != photo_urls[0]
+                        or current_additional != photo_urls[1:]
+                    ):
+                        product.primary_image = photo_urls[0]
+                        product.additional_images = photo_urls[1:]
+                        logger.info(
+                            "Updated product %s media from Reverb listing %s",
+                            product.sku,
+                            reverb_id,
+                        )
+                        await self.db.flush()
 
             # Upsert platform_common
             existing_common_query = select(PlatformCommon).where(
